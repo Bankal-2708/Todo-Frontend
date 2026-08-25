25540bb5327785001a98bdc7ca83828879b4cd2f
Bankal-2708 <bankalmavi1@gmail.com>
2026-08-26 00:08:05 +0530

    config file update

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_BASE_URL = 'https://backend-gamma-umber-41.vercel.app'
+
 const api = axios.create({
-  baseURL: '/',
+  baseURL: API_BASE_URL,
 })
 
 export default api
